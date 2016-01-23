a97289e1c7c9da9436d593e783fc0ad60a011b0e
Patrick Grossmann <patrick@growthbox.com.au>
2016-01-23 16:01:56 +1100

    Latest changes to Frontpage. Error detected on XS screen size. XS is not responsive.

--- mainsite/_site/config.rb
+++ mainsite/_site/config.rb
@@ -8,6 +8,7 @@ sass_dir = "_sass"
 images_dir = "images"
 javascripts_dir = "js"
 
+
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
 
